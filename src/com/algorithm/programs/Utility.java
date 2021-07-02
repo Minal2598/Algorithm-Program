c46db8f9ec453f35ef3731fbc89913712dd1e49f
Minal <patilminalp@gmail.com>
2021-07-03 01:09:24 +0530

    Added:Detect two Strings are Anagram or not.

--- src/com/algorithm/programs/Utility.java
+++ src/com/algorithm/programs/Utility.java
@@ -121,7 +121,25 @@ public class Utility {
 		}
 		return returnArray;
 	}
+	
+			//Anagram
 
+	public boolean anagram(String string1, String string2) {
+		if(string1.length() != string2.length()) {
+			return false;
+		}
+		char[] array1 = string1.toCharArray();
+		Arrays.sort(array1);
+		char[] array2 = string2.toCharArray();
+		Arrays.sort(array2);
+		
+		for(int i = 0; i < array1.length; i++) {
+			if(array1[i] != array2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 
